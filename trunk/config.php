c6d1c77838a98d5bf8a6aa733291f0d2bc35f730
byrnereese <byrnereese@011cf080-c167-11dd-8240-87c3ad04ac65>
2008-12-03 18:31:46 +0000

    initial addition and basic refactoring of twitster for packaging and beta

---
+++ trunk/config.php
@@ -0,0 +1,29 @@
+<?php
+// The hashtag to filter by:
+define('HASHTAG', '#mt');
+
+// Turn on/off caching of the first page.
+define("CACHE_ENABLED", 1);
+// The filename in which to store the cached HTML version.
+define("CACHE_INDEX",'cache/cached_page.html');
+// The filename in which to store the cached RSS feed.
+define("CACHE_FEED",'cache/cached_feed.rss');
+// The number of seconds to cache the homepage and feed.
+define("CACHE_TIME",60 * 5); // Clear cache ever 3 Minutes
+
+// Database connection information. These should be obvios to most.
+define("TWITSTER_DBHOST","localhost");
+define("TWITSTER_DBNAME","hashmt");
+define("TWITSTER_DBUSER","root");
+define("TWITSTER_DBPASS","");
+
+// The number of Tweets to display per page.
+define("PAGE_LIMIT",20);
+
+// Turn on/off debug messages to assist in development.
+define("DEBUG",0);
+
+// Used to specify the URL string length limit to the Twitter
+// Search API. Deprecated.
+define("QUERY_LIMIT",139);
+?>
