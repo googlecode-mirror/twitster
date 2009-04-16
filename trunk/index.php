b797fbbe766a322c0c958736913ca82e284175c6
theplasticmind <theplasticmind@011cf080-c167-11dd-8240-87c3ad04ac65>
2009-04-16 20:14:15 +0000

    Context sensitive feed links now allow for subscribing to a filtered tweet stream.

--- trunk/index.php
+++ trunk/index.php
@@ -10,15 +10,14 @@ $twitster = new twitster();
 $twitster->init();
 
 $offset = isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0;
-$tag    = isset($_REQUEST['tag']);
-if (!isset($tag) && HASHTAG) { $tag = HASHTAG; }
+if (!isset($_REQUEST['tag']) && HASHTAG) { $tag = HASHTAG; }
+$feed_link='rss.php?tag='.urlencode($tag);
 $options = array();
 $options['tag']    = $tag;
 $options['offset'] = $offset;
 $options['limit']  = PAGE_LIMIT;
 
 if ($offset == 0) { do_update_if_needed($twitster); }
-
 $tweets = Tweet::find($options);
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -31,7 +30,7 @@ $tweets = Tweet::find($options);
    	<link rel="stylesheet" href="themes/mellow/css/twitster-mellow.css" type="text/css" media="screen, projection" />
    	<link rel="icon" href="themes/mellow/i/favicon.ico" type="image/x-icon" />
    	<link rel="shortcut icon" href="themes/mellow/i/favicon.ico" type="image/x-icon" />
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="rss.php" />
+	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo $feed_link; ?>" />
 </head>
 
 <body>
@@ -42,7 +41,7 @@ $tweets = Tweet::find($options);
 				<h2><?php echo SITE_SUBTITLE ?></h2>
 			</div>
 			<div id="content">
-				<div class="feedicon"><a href="rss.php" title="Subscribe to <?php echo SITE_TITLE ?> via RSS"><img src="themes/mellow/i/rss.png" alt="Feed Icon"></a></div>
+				<div class="feedicon"><a href="<?php echo $feed_link; ?>" title="Subscribe to <?php echo SITE_TITLE ?> via RSS"><img src="themes/mellow/i/rss.png" alt="Feed Icon"></a></div>
 				<ul id="updatelist">
 					<?php
 					  $i=0;
